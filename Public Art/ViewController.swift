27991a2ad9151afb672af74115d1e62075c884d1
Tom Kearsley <Thomas@Toms-MacBook.local>
2016-11-08 09:20:50 +1300

    Initial Commit

---
+++ Public Art/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Public Art
+//
+//  Created by Tom Kearsley on 8/11/16.
+//  Copyright © 2016 Tom Kearsley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
